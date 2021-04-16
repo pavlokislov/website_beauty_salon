8ab18d31425c4b011fc439e45749a14d41139c56
pavlokislov <kislovpasha@gmail.com>
2021-04-16 17:45:48 +0300

    Upload website beauty salon

--- src/main/java/com/website/spring/service/controllers/BookingController.java
+++ src/main/java/com/website/spring/service/controllers/BookingController.java
@@ -31,7 +31,6 @@ public class BookingController {
     @Autowired
     private MasterScheduleRepository masterScheduleRepository;
 
-
     private BookingDto wholeBookingDto;
 
     @GetMapping("/booking")
